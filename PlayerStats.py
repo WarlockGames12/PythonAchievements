c175d85c57b66108ff4c583de0d7ada49cada7f7
WarlockGames12 <fpweeyou@gmail.com>
2020-09-18 12:31:41 +0200

    STANDPOWER

---
+++ PlayerStats.py
@@ -0,0 +1,13 @@
+Name = "Dirk ten Have"
+StandName = "Parasyte"
+Power = "C"
+Speed = "A"
+Range = "A"
+Durability = "D"
+Precision = "A"
+Potential = "C"
+Ability = "Mind Manupilation with game controller"
+Ability2 = "Analyse every stand user"
+Ability3 = "Energy Burst"
+
+print('His name is ' + Name + ' He has a stand named ' + StandName + ', His power level is ' + Power + ', his stand speed level is ' + Speed + ', his range level is ' + Range + ', his durability level is ' + Durability + ', His precision level is ' + Precision + ', his potential level is ' + Potential + ', His first stand ability is ' + Ability + ', His second Ability is ' + Ability2 + ' and his third ability is ' + Ability3)   
