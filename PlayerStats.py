4e9a52c87403acc7f2c4e579ecee3d388fb627a7
WarlockGames12 <fpweeyou@gmail.com>
2020-09-25 11:41:23 +0200

    yay

--- PlayerStats.py
+++ PlayerStats.py
@@ -6,8 +6,11 @@ Range = "A"
 Durability = "D"
 Precision = "A"
 Potential = "C"
+Ability4 = 3
 Ability = "Mind Manupilation with game controller"
 Ability2 = "Analyse every stand user"
 Ability3 = "Energy Burst"
+Standuser = {"name" : "Dirk ten Have", "age" : 18}
 
-print('His name is ' + Name + ' He has a stand named ' + StandName + ', His power level is ' + Power + ', his stand speed level is ' + Speed + ', his range level is ' + Range + ', his durability level is ' + Durability + ', His precision level is ' + Precision + ', his potential level is ' + Potential + ', His first stand ability is ' + Ability + ', His second Ability is ' + Ability2 + ' and his third ability is ' + Ability3)   
+print(Standuser)
+print('His name is ' + Name + ', He has a stand named ' + StandName + ', His power level is ' + Power + ', his stand speed level is ' + Speed + ', his range level is ' + Range + ', his durability level is ' + Durability + ', His precision level is ' + Precision + ', his potential level is ' + Potential + ', He has ' + str(Ability4) + ' abilities' + ', His first stand ability is ' + Ability + ', His second Ability is ' + Ability2 + ' and his third ability is ' + Ability3)   
