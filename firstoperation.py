59a0b34ad54a1eddc5f80b097112c074678297cd
WarlockGames12 <fpweeyou@gmail.com>
2020-09-25 12:13:23 +0200

    firstoperation

---
+++ firstoperation.py
@@ -0,0 +1,21 @@
+som = 2
+som2 = 3
+som3 = som + som2
+som4 = som2 - som
+som5 = som2 / som
+som6 = som3 * som2
+
+print(str(som3))
+print(str(som4))
+print(str(som5))
+print(str(som6))
+som += 5
+print(str(som))
+som2 /= 2
+print(str(som2))
+som3 % 3
+print(str(som3)) 
+som4 -= 4
+print(str(som4))
+som5 *= 5
+print(str(som5))
