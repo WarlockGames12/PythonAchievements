7503870cf340660435693e097e9aeb9c5aff00d5
WarlockGames12 <fpweeyou@gmail.com>
2020-10-22 11:45:12 +0200

    yay

---
+++ FamilyTies.py
@@ -0,0 +1,10 @@
+naam = "Dirk"
+leeftijd = 19
+woonplaats = "Maastricht"
+kmnaarschool = 205
+favourietegame = "Pokemon"
+favourieteanime = "Jojo's Bizarre Adventure"
+hobby = "tekenen, gamen en games ontwikkelen"
+
+print('Mijn naam is ' + naam + ', ik ben ' + str(leeftijd) + ' jaar oud en ik woon in ' + woonplaats + ', ik woon ' + str(kmnaarschool) + ' km van mijn school vandaan')
+print('Mijn favouriete game is ' + favourietegame + ' en mijn favouriete anime is ' + favourieteanime + ', mijn hobbies zijn ' + hobby)
