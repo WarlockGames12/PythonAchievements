7f4f3ee855536b5fed6b2e8dc3a7a1278a19223a
WarlockGames12 <fpweeyou@gmail.com>
2020-10-23 12:26:11 +0200

    school

---
+++ school.py
@@ -0,0 +1,59 @@
+# Verander in False als je geen schooldag hebt
+schooldagMaandag = True
+
+if schooldagMaandag == True:
+  print("Pak je spullen en ga er heen!")
+else:
+  print("Lekker uitslapen!")
+
+schooldagDinsdag = True
+
+if schooldagDinsdag == True:
+  print("Pak je spullen en ga er heen!")
+else:
+  print("Lekker uitslapen!")
+
+
+schooldagWoensdag = False
+
+if schooldagWoensdag == True:
+  print("Pak je spullen en ga er heen!")
+else:
+  print("Lekker uitslapen!")
+
+
+schooldagDonderdag = True
+
+if schooldagDonderdag == True:
+  print("Pak je spullen en ga er heen!")
+else:
+  print("Lekker uitslapen!")
+
+
+schooldagVrijdag = True
+
+if schooldagVrijdag == True:
+  print("Pak je spullen en ga er heen!")
+else:
+  print("Lekker uitslapen!")
+
+
+schooldagZaterdag = False
+
+if schooldagZaterdag == True:
+  print("Pak je spullen en ga er heen!")
+else:
+  print("Lekker uitslapen!")
+
+
+schooldagZondag = False
+
+if schooldagZondag == True:
+  print("Pak je spullen en ga er heen!")
+else:
+  print("Lekker uitslapen!")
+
+
+
+
+
