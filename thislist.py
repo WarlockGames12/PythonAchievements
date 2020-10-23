551516cb96dddbe6403d94a401f8211d93d419cc
WarlockGames12 <fpweeyou@gmail.com>
2020-10-23 12:39:22 +0200

    list

---
+++ thislist.py
@@ -0,0 +1,8 @@
+thislist = ["apple", "banana", "cherry", "pear", "pineapple"]
+print(thislist)
+thislist.reverse()
+print(thislist)
+thislist.sort()
+print(thislist)
+thislist.remove("apple")
+print(thislist)
